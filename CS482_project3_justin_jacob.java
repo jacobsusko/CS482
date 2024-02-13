229e5c41908b7861b73c5d44c5e6a963256bbd36
Jacob <susko@dukes.jmu.edu>
2024-02-13 16:55:30 -0500

    variables added

--- CS482_project3_justin_jacob.java
+++ CS482_project3_justin_jacob.java
@@ -1,3 +1,4 @@
+
 /**
  * @author Jacob Susko
  * @author Justin Lacombe
@@ -5,5 +6,10 @@
  */
 
 public class CS482_project3_justin_jacob {
-    
+    private int iv = 0xC17728A947CCC9EE21B4EAED9C62C888;
+    private int c1 = 0x62FD8D388D65839F79E8A9382B0B14F3;
+    private int c2 = 0xE8C955F4864CAE8A0930FF3B73F38852;
+    private int c3 = 0x4F2971D6351B2052C41C0EF93DAEDC47;
+    private int c4 = 0x8B12D7F4686BDEE986CAFF6C7507851E;
+
 }
