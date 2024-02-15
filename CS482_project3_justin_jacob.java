560e9249ddac62e17f12e6d39fcdcd514d433dbf
Jacob <susko@dukes.jmu.edu>
2024-02-15 15:19:42 -0500

    Brute force key

--- CS482_project3_justin_jacob.java
+++ CS482_project3_justin_jacob.java
@@ -13,8 +13,11 @@ public class CS482_project3_justin_jacob {
     // Need to figure out how to find the key
 
 
-    private static String val = "00000000000000000000000000000000011000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000011";
-    private static byte[] knownKeyPart = new BigInteger(val, 2).toByteArray(); // 0 - 32 index are unknown and can be random
+
+    private static byte[] inkey = {(byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, 
+        (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, 
+        (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, 
+        (byte) 0x00, (byte) 0x03}; // 5th byte is either 6 or f
 
     private static byte[] iv = {(byte) 0xC1, (byte) 0x77, (byte) 0x28, (byte) 0xA9, 
         (byte) 0x47, (byte) 0xCC, (byte) 0xC9, (byte) 0xEE, (byte) 0x21, 
@@ -39,102 +42,20 @@ public class CS482_project3_justin_jacob {
 
 
     public static void main(String[] args) {
-        for (int i = 0; i < 16; i++) {  // 16 might need to get changed to 255 because each byte could at most be 0xff
-            knownKeyPart[0] = (byte) i;
-            for (int j = 0; j < 16; j++) {
-                knownKeyPart[1] = (byte) j;
-                for (int k = 0; k < 16; k++) {
-                    knownKeyPart[2] = (byte) k;
-                    for (int l = 0; l < 16; l++) {
-                        knownKeyPart[3] = (byte) l;
-                        for (int z = 0; z < 16; z++) {
-                            knownKeyPart[4] = (byte) z;
-                            for (int x = 0; x < 16; x++) {
-                                knownKeyPart[5] = (byte) x;
-                                for (int c = 0; c < 16; c++) {
-                                    knownKeyPart[6] = (byte) c;
-                                    for (int v = 0; v < 16; v++) {
-                                        knownKeyPart[7] = (byte) v;
-                                        for (int b = 0; b < 16; b++) {
-                                            knownKeyPart[8] = (byte) b;
-                                            for (int n = 0; n < 16; n++) {
-                                                knownKeyPart[9] = (byte) n;
-                                                for (int m = 0; m < 16; m++) {
-                                                    knownKeyPart[10] = (byte) m;
-                                                    for (int a = 0; a < 16; a++) {
-                                                        knownKeyPart[11] = (byte) a;
-                                                        for (int s = 0; s < 16; s++) {
-                                                            knownKeyPart[12] = (byte) s;
-                                                            for (int d = 0; d < 16; d++) {
-                                                                knownKeyPart[13] = (byte) d;
-                                                                for (int f = 0; f < 16; f++) {
-                                                                    knownKeyPart[14] = (byte) f;
-                                                                    for (int g = 0; g < 16; g++) {
-                                                                        knownKeyPart[15] = (byte) g;
-                                                                        for (int h = 0; h < 16; h++) {
-                                                                            knownKeyPart[16] = (byte) h;
-                                                                            for (int q = 0; q < 16; q++) {
-                                                                                knownKeyPart[17] = (byte) q;
-                                                                                for (int w = 0; w < 16; w++) {
-                                                                                    knownKeyPart[18] = (byte) w;
-                                                                                    for (int e = 0; e < 16; e++) {
-                                                                                        knownKeyPart[19] = (byte) e;
-                                                                                        for (int r = 0; r < 16; r++) {
-                                                                                            knownKeyPart[20] = (byte) r;
-                                                                                            for (int t = 0; t < 16; t++) {
-                                                                                                knownKeyPart[21] = (byte) t;
-                                                                                                for (int y = 0; y < 16; y++) {
-                                                                                                    knownKeyPart[22] = (byte) y;
-                                                                                                    for (int u = 0; u < 16; u++) {
-                                                                                                        knownKeyPart[23] = (byte) u;
-                                                                                                        for (int o = 0; o < 16; o++) {
-                                                                                                            knownKeyPart[24] = (byte) o;
-                                                                                                            for (int p = 0; p < 16; p++) {
-                                                                                                                knownKeyPart[25] = (byte) p;
-                                                                                                                for (int qq = 0; qq < 16; qq++) {
-                                                                                                                    knownKeyPart[26] = (byte) qq;
-                                                                                                                    for (int ww = 0; ww < 16; ww++) {
-                                                                                                                        knownKeyPart[27] = (byte) ww;
-                                                                                                                        for (int ee = 0; ee < 16; ee++) {
-                                                                                                                            knownKeyPart[28] = (byte) ee;
-                                                                                                                            for (int rr = 0; rr < 16; rr++) {
-                                                                                                                                knownKeyPart[29] = (byte) rr;
-                                                                                                                                for (int tt = 0; tt < 16; tt++) {
-                                                                                                                                    knownKeyPart[30] = (byte) tt;
-                                                                                                                                    for (int yy = 0; yy < 16; yy++) {
-                                                                                                                                        knownKeyPart[31] = (byte) yy;
-                                                                                                                                        for (int ii = 0; ii < 16; ii++) {
-                                                                                                                                            knownKeyPart[32] = (byte) ii;
-                                                                                                                                            // call decrypt with each possible symmetric key
-                                                                                                                                            // and print out with key until clear text is found
-                                                                                                                                        }
-                                                                                                                                    }
-
-                                                                                                                                }
-                                                                                                                            }
-                                                                                                                        }
-                                                                                                                    }
-                                                                                                                }
-                                                                                                            }
-                                                                                                        }
-                                                                                                    }
-                                                                                                }
-                                                                                            }
-                                                                                        }
-                                                                                    }
-                                                                                }
-                                                                            }
-                                                                        }
-                                                                    }
-                                                                }
-                                                            }
-                                                        }
-                                                    }
-                                                }
-                                            }
-                                        }
-                                    }
-                                }
+        // for loop going through each byte possibility
+        for (byte i = (byte) 0x00; i <= (byte) 0xff; i++) {
+            inkey[0] = i;
+            for (byte j = (byte) 0x00; j <= (byte) 0xff; j++) {
+                inkey[1] = j;
+                for (byte k = (byte) 0x00; k <= (byte) 0xff; k++) {
+                    inkey[2] = k;
+                    for (byte z = (byte) 0x00; z <= (byte) 0xff; z++) {
+                        inkey[3] = z;
+                        for (int g = 0; g < 2; g++) {
+                            if (g == 0) {
+                                inkey[4] = (byte) 0x60;
+                            } else {
+                                inkey[4] = (byte) 0xf0;
                             }
                         }
                     }
