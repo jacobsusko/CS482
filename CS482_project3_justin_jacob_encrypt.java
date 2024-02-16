dfd178a057fe9ac0fe5a41c63f602380c2f616df
Jacob <susko@dukes.jmu.edu>
2024-02-16 08:54:50 -0500

    key

--- CS482_project3_justin_jacob_encrypt.java
+++ CS482_project3_justin_jacob_encrypt.java
@@ -1,3 +1,24 @@
+import java.math.BigInteger;
+
+/**
+ * @author Jacob Susko
+ * @author Justin Lacombe
+ * @date 2/16/2024
+ */
+
 public class CS482_project3_justin_jacob_encrypt {
-    
+
+
+
+
+    public static void main(String[] args) {
+        String k = "DC37471D600000000000000000000003";
+        BigInteger k2 =  new BigInteger(k, 16);
+        byte[] kTemp =  k2.toByteArray();
+        byte[] key = new byte[80];
+        for (int p = 1; p < kTemp.length; p++) {
+            key[p-1] = kTemp[p];
+        }
+    }
+
 }
